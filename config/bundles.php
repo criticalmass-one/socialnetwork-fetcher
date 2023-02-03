90d90113db3cd60e8314b2e93d88b3b129b4eabf
Malte Hübner <maltehuebner@gmx.org>
2023-02-03 13:06:58 +0100

    Update to Symfony 6.2

--- config/bundles.php
+++ config/bundles.php
@@ -2,6 +2,5 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
